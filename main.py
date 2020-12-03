4d59f21294ca79dcce7037cd3de0403976858917
Meet Parikh <meetparikh7@gmail.com>
2020-12-04 01:20:09 +0530

    Add friendly errors and allow port reuse

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ PORT = 65431        # The port used by the server
 
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        while True:
-            print("prompt>", end=" ")
-            command = input()
-            util.send(s, command)
-            if command.strip() == "exit":
-                break
-            print(util.recv(s))
+        try:
+            s.connect((HOST, PORT))
+            while True:
+                print("prompt>", end=" ")
+                command = input()
+                util.send(s, command)
+                if command.strip() == "exit":
+                    break
+                print(util.recv(s))
+        except:
+            print("Server broke connection unexpectedly")
+            import traceback; traceback.print_exc()
+        finally:
+            s.close()
--- server.py
+++ server.py
@@ -8,27 +8,43 @@ HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65431  # Port to listen on (non-privileged ports are > 1023)
 BASEPATH = "test_dir"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_serv:
+
+def execute_command(command):
+    if command[0] == "ls":
+        long = False
+        if "-l" in command:
+            long  = True
+        return commands.ls(BASEPATH, long)
+    elif command[0] == "hash":
+        return commands.hashfile(BASEPATH, command[1])
+
+
+def main(socket_serv):
+    socket_serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     socket_serv.bind((HOST, PORT))
     socket_serv.listen()
     sock, addr = socket_serv.accept()
-    with sock:
+    try:
         print("Connected by", addr)
         while True:
             command = util.recv(sock)
             command = command.strip().split(" ")
             command = list(filter(lambda v: v, command))
-            toret = ""
-            if command[0] == "ls":
-                long = False
-                if "-l" in command:
-                    long  = True
-                toret = commands.ls(BASEPATH, long)
-            elif command[0] == "hash":
-                toret = commands.hashfile(BASEPATH, command[1])
-            elif command[0] == "exit":
+            toret = "Invalid command!"
+            if command[0] == "exit":
                 break
+            else:
+                toret = execute_command(command)
             util.send(sock, toret)
+    finally:
+        sock.close()
 
 if __name__ == "__main__":
-    pass
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_serv:
+        try:
+            main(socket_serv)
+        except:
+            print("Unexpected error encountered")
+            import traceback; traceback.print_exc()
+        finally:
+            socket_serv.close()
